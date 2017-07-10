df160d6ce24fbae08e78b36cd871dbde112f31cb
Daniel <danrpts@gmail.com>
2017-07-09 20:34:00 -0700

    updated cli and helpers; adding semicolons back; async wrap

--- examples/cli.js
+++ examples/cli.js
@@ -4,13 +4,17 @@ const _ = require('underscore');
 const minimist = require('minimist');
 const readline = require('readline');
 const ip = require('ip');
-const Bucket = require('../lib/bucket.js').Bucket
+const Bucket = require('../lib/bucket.js').Bucket;
 const utils = require('../lib/utils.js');
-const isPort = utils.isPort
-const isAddress = utils.isAddress
-const arg0 = 'chord'
+const isPort = utils.isPort;
+const isAddress = utils.isAddress;
+const toStateString = utils.toStateString;
+const arg0 = 'chord';
+
+// parse command line arguments
 const argv = minimist(process.argv.slice(2));
 
+// remove redundant white space
 argv._ = _.compact(argv._);
 
 function printUsage () {
@@ -18,28 +22,6 @@ function printUsage () {
   var spacer = ' '.repeat(arg0.length);
   console.error(`Usage: ${arg0} [--version] [--help] [-p <port>]`);
   console.error(`       ${spacer} [-r <replicas>] [--join=<address>]\n`);
-
-}
-
-function printState (bucket, withFingers = false) {
-
-  var info = '';
-  info += `<Predecessor> ${(bucket.predecessor) ? bucket.predecessor : ''}\n`;
-  info += `<Self> ${bucket.address}\n`;
-  
-  var successor = bucket.successor;
-  for (let i in successor) {
-    info += `<Successor ${i}> ${successor[i]}\n`;
-  }
-
-  if (withFingers) {
-    let finger = bucket.finger
-    for (let i in finger) {
-      info += `<Finger ${i}> ${finger[i]}\n`;
-    }
-  }
-
-  console.error(info);
 
 }
 
@@ -58,12 +40,11 @@ function printHelp () {
 
   console.error('Commands:');
   console.error(' join <address>            Add this peer to a network.');
-  console.error(' leave                     Remove this peer from the network.');
   console.error(' get <key>                 Read a value from network.');
   console.error(' set <key> [value]         Create/update a key in the network.');
   console.error(' del <key>                 Delete a key and value from the network.');
   console.error(' ping <address>            Ping a remote peer.');
-  console.error(' state [-f]                Print info about this peer.');
+  console.error(' state [address] [-f]      Print state information.');
   console.error(' bucket [address]          Print bucket contents.')
   console.error(' quit                      Leave the network and exit.');
   console.error(' help                      Show this screen.\n');
@@ -105,231 +86,249 @@ if (argv._.length > 0
 // start cli
 } else {
 
-  const bucket = new Bucket({
-    nSuccessors: argv.r
-  });
+  (async () => {
 
-  try {
-    bucket.listen(argv.p, ip.address());
-  } catch (e) {
-    console.error(e);
-    process.exit(1);  
-  } 
+    const bucket = new Bucket({
+      nSuccessors: argv.r
+    });
 
-  // define command interface
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "> "
-  });
+    bucket.on('successor::up', up => {
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      console.log(`UP ${up}`);
+      rl.prompt(true);
+    });
 
-  // check for join argument
-  if (isAddress(argv.join)) {
-    bucket.join(argv.join);
-  }
+    bucket.on('successor::down', down => {
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      console.log(`DOWN ${down}`);
+      rl.prompt(true);
+    });
 
-  rl.prompt();
-
-  rl.on('line', line => {
-
-    if (!line) {
-      rl.prompt();
-      return
+    try {
+      await bucket.listen(argv.p, ip.address());
+      if (_.has(argv, 'join')) {
+        await bucket.join(argv.join);
+      }
+    } catch (e) {
+      console.error(e);
+      process.exit(1);  
     }
 
-    let [command, ...args] = line.trim().split(' ');
-    args = minimist(args);
-    args._ = _.compact(args._);
+    // define command interface
+    const rl = readline.createInterface(process.stdin, process.stdout);
 
-    // TODO
-    // handle empty line
+    rl.prompt();
 
-    switch (command) {
+    rl.on('close', () => {
+      process.exit(0);
+    });
 
-      case 'ping':
+    rl.on('line', line => {
 
-        var address = args._[0]
-        var t = process.hrtime();
+      if (!line) {
+        rl.prompt();
+        return
+      }
 
-        if (!isAddress(address)) {
-          printHelp();
-          rl.prompt();
-          break
-        }
+      let [command, ...args] = line.trim().split(' ');
+      args = minimist(args);
+      args._ = _.compact(args._);
 
-        (async () => {
-          try {
-            await bucket.ping(address);
-            t = process.hrtime(t);
-            console.log(`<Ping ${address}> ${(t[1] / 1e6).toPrecision(3)} ms`);
-          } catch (e) {
-            console.error(e);
-          } finally {
+      // TODO
+      // handle empty line
+
+      switch (command) {
+
+        case 'ping':
+
+          var host = args._[0];
+          var t = process.hrtime();
+
+          if (!isAddress(host)) {
+            printHelp();
             rl.prompt();
+            break;
           }
-        })();
-        break
 
-      case 'join':
-        
-        var address = args._[0]
+          (async () => {
+            try {
+              let t = await bucket.ping(host);
+              console.log(`<Ping ${host}> ${(t[1] / 1e6).toPrecision(3)} ms`);
+            } catch (e) {
+              console.error(e);
+            } finally {
+              rl.prompt();
+            }
+          })();
+          break;
 
-        if (!isAddress(address)) {
-          printHelp();
-          rl.prompt();
-          break
-        }
-
-        (async () => {
-          try {
-            await bucket.join(address);
-          } catch (e) {
-            console.error(e);
-          } finally {
-            rl.prompt();
-          }
-        })();
-        break
-
-      case 'leave':
-
-        (async () => {
-          try {
-            await bucket.leave();
-          } catch (e) {
-            console.error(e);
-          } finally {
-            rl.prompt();          
-          }
-        })();
-        break
-
-      case 'get':
-
-        var key = args._[0]
-
-        if (!_.isString(key)) {
-          printHelp();
-          rl.prompt();
-          break
-        }
-
-        (async () => {
-          try {
-            let value = await bucket.get(key);
-            console.log(`<Entry ${key}> ${value.toString('utf8')}`);
-          } catch (e) {
-            console.error(e);
-          } finally {
-            rl.prompt();
-          }
-        })();
-        break
-
-      case 'set':
-
-        var key = args._[0]
-        var value = Buffer.from(args._.slice(1).join(' '));
-
-        if (!_.isString(key)) {
-          printHelp();
-          rl.prompt();
-          break
-        }
-
-        (async () => {
-          try {
-           await bucket.set(key, value);
-          } catch (e) {
-            console.error(e);
-          } finally {
-            rl.prompt();
-          }
-        })();
-        break
-
-      case 'del':
-
-        var key = args._[0]
-
-        if (!_.isString(key)) {
-          printHelp();
-          rl.prompt();
-          break
-        }
+        case 'join':
           
-        (async () => {
-          try {
-            await bucket.del(key);
-          } catch (e) {
-            console.error(e);
-          } finally {
+          var host = args._[0];
+
+          if (!isAddress(host)) {
+            printHelp();
             rl.prompt();
+            break;
           }
-        })();
-        break
 
-      case 'state':
-        
-        printState(bucket, args.f);
-        rl.prompt();
-        break
+          (async () => {
+            try {
+              await bucket.join(host);
+            } catch (e) {
+              console.error(e);
+            } finally {
+              rl.prompt();
+            }
+          })();
+          break;
 
-      case 'bucket':
+        case 'get':
 
-        var address = args._[0]
+          var key = args._[0];
 
-        // print local
-        if (!isAddress(address)) {
-          printBucket(bucket.hashtable);
+          if (!_.isString(key)) {
+            printHelp();
+            rl.prompt();
+            break;
+          }
+
+          (async () => {
+            try {
+              let value = await bucket.get(key);
+              console.log(`<Entry ${key}> ${value.toString('utf8')}`);
+            } catch (_) {
+              console.error(`<Entry ${key}> undefined`);
+            } finally {
+              rl.prompt();
+            }
+          })();
+          break;
+
+        case 'set':
+
+          var key = args._[0];
+          var value = Buffer.from(args._.slice(1).join(' '));
+
+          if (!_.isString(key)) {
+            printHelp();
+            rl.prompt();
+            break;
+          }
+
+          (async () => {
+            try {
+             await bucket.set(key, value);
+            } catch (e) {
+              console.error(e);
+            } finally {
+              rl.prompt();
+            }
+          })();
+          break;
+
+        case 'del':
+
+          var key = args._[0];
+
+          if (!_.isString(key)) {
+            printHelp();
+            rl.prompt();
+            break;
+          }
+            
+          (async () => {
+            try {
+              await bucket.del(key);
+            } catch (e) {
+              console.error(e);
+            } finally {
+              rl.prompt();
+            }
+          })();
+          break;
+
+        case 'state':
+          
+          var host = args._[0];
+
+          // print local
+          if (!isAddress(host)) {
+            let state = toStateString(bucket, args.f);
+            console.log(state);
+            rl.prompt();
+            break;
+          }
+          // print remote
+          (async () => {
+            try {
+              let response = await bucket.state(host, args.f);
+              response.address = host;
+              let state = toStateString(response);
+              console.log(state);
+            } catch (e) {
+              console.error(e);
+            } finally {
+              rl.prompt();
+            }
+          })();
+          break;
+
+        case 'bucket':
+
+          var host = args._[0];
+
+          // print local
+          if (!isAddress(host)) {
+            printBucket(bucket.hashtable);
+            rl.prompt();
+            break;
+          }
+
+          // print remote
+          (async () => {
+            try {
+              let contents = await bucket.all(host);
+              printBucket(contents);
+            } catch (e) {
+              console.error(e);
+            } finally {
+              rl.prompt();
+            }
+          })();
+          break;
+
+        case 'quit':
+
+          (async () => {
+            try {
+              bucket.close();
+            } catch (e) {
+              console.error(e);
+            } finally {
+              process.exit(0);
+            }
+          })();
+          break;
+
+        case 'help':
+
+          printHelp();
           rl.prompt();
-          break
-        }
+          break;
 
-        // print remote
-        (async () => {
-          try {
-            let contents = await bucket.all(address);
-            printBucket(contents);
-          } catch (e) {
-            console.error(e);
-          } finally {
-            rl.prompt();
-          }
-        })();
-        break
+        default: 
 
-      case 'quit':
+          printHelp();
+          rl.prompt();
+          break;
+      
+      }
 
-        (async () => {
-          try {
-            bucket.close();
-          } catch (e) {
-            console.error(e);
-          } finally {
-            process.exit(0);
-          }
-        })();
-        break
+    });
 
-      case 'help':
-
-        printHelp();
-        rl.prompt();
-        break
-
-      default: 
-
-        printHelp();
-        rl.prompt();
-        break
-    
-    }
-
-  });
-
-  rl.on('close', () => {
-    process.exit(0);
-  });
+  })();
 
 }
